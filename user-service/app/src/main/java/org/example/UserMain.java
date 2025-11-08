3e64993036aafb5d8ddb7a8ad688c30ab1bfd6d8
Nesch595 <jack.gokcver9564@gmail.com>
2025-11-08 16:57:50 +0300

    Refactor: restructure project - move src to root and fix Gradle config

--- user-service/app/src/main/java/org/example/UserMain.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class UserMain {
-    public static void main(String[] args) {
-        System.out.println("Success");
-    }
-}
---
+++ user-service/src/main/java/org/example/entity/User.java
@@ -0,0 +1,44 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    // Один пользователь может иметь несколько адресов
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Address> addresses;
+
+    // Один пользователь может иметь несколько ролей, и наоборот
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+}
