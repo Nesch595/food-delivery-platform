955695cc29e1368fc7d9f376eb36247affa88de0
Nesch595 <jack.gokcver9564@gmail.com>
2025-11-10 08:45:52 +0300

    Fix: misspells in changelogs

--- order-service/src/main/resources/db/changelog/changes/005-change-type-order-total-price.sql
+++ order-service/src/main/resources/db/changelog/changes/005-change-type-order-total-price.sql
@@ -1,5 +1,5 @@
 -- Изменить тип total_price по аналогии с order_item
-ALTER TABLE public.orders
+ALTER TABLE public.order
 ALTER COLUMN total_price TYPE NUMERIC(10,2),
 ALTER COLUMN total_price SET DEFAULT 0.00,
 ALTER COLUMN total_price SET NOT NULL;
@@ -10,5 +10,5 @@ DROP CONSTRAINT IF EXISTS order_item_fk;
 
 ALTER TABLE public.order_item
 ADD CONSTRAINT order_item_order_fk
-FOREIGN KEY (order_id) REFERENCES public.orders(id)
+FOREIGN KEY (order_id) REFERENCES public.order(id)
 ON DELETE CASCADE ON UPDATE CASCADE;
