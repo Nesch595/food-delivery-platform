86db8ce923d2ae0af27b1c1e1704c68db622d3b9
Nesch595 <jack.gokcver9564@gmail.com>
2025-11-10 08:35:54 +0300

    Add: support for decimal prices

---
+++ order-service/src/main/resources/db/changelog/changes/005-change-type-order-total-price.sql
@@ -0,0 +1,14 @@
+-- Изменить тип total_price по аналогии с order_item
+ALTER TABLE public.orders
+ALTER COLUMN total_price TYPE NUMERIC(10,2),
+ALTER COLUMN total_price SET DEFAULT 0.00,
+ALTER COLUMN total_price SET NOT NULL;
+
+-- Обновить foreign key в order_item
+ALTER TABLE public.order_item
+DROP CONSTRAINT IF EXISTS order_item_fk;
+
+ALTER TABLE public.order_item
+ADD CONSTRAINT order_item_order_fk
+FOREIGN KEY (order_id) REFERENCES public.orders(id)
+ON DELETE CASCADE ON UPDATE CASCADE;
